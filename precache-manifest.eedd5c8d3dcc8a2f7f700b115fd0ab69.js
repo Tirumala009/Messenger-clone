3fc9c77986c4f120cab44dc245b4f970bd4b0817
Tirumala009 <69470807+Tirumala009@users.noreply.github.com>
2020-08-28 22:38:13 +0100

    Updates

---
+++ precache-manifest.eedd5c8d3dcc8a2f7f700b115fd0ab69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8634ab3408287442fb15c40fb16d68d",
+    "url": "/Messenger-clone/index.html"
+  },
+  {
+    "revision": "1372a7e78bb79b89115e",
+    "url": "/Messenger-clone/static/css/main.de14401d.chunk.css"
+  },
+  {
+    "revision": "20412ee92b64222f5903",
+    "url": "/Messenger-clone/static/js/2.4bf427dd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Messenger-clone/static/js/2.4bf427dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1372a7e78bb79b89115e",
+    "url": "/Messenger-clone/static/js/main.50ca7a7a.chunk.js"
+  },
+  {
+    "revision": "33f1e9e13616c5d4df3f",
+    "url": "/Messenger-clone/static/js/runtime-main.4be2e6b7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Messenger-clone/precache-manifest.eedd5c8d3dcc8a2f7f700b115fd0ab69.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Messenger-clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
